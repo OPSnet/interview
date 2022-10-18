19ba5ad36e71d7a1db397eb77771b088f396637a
Noobgamer0111 <aadilsyed5117@gmail.com>
2022-10-19 10:47:47 +1100

    Update cd-burning-and-cd-ripping.php
    Minor change in misspelling. In line 7, the title, "CD burning and CD ripping" is written as "CD bburning and CD ripping".

--- cd-burning-and-cd-ripping.php
+++ cd-burning-and-cd-ripping.php
@@ -4,7 +4,7 @@
 
 newstop();
 
-newsbox('CD bburning and CD ripping','<strong>CD ripping</strong> is a way to extract the music files from a CD. <strong>CD burning</strong> is a way to make a CD from music files.
+newsbox('CD burning and CD ripping','<strong>CD ripping</strong> is a way to extract the music files from a CD. <strong>CD burning</strong> is a way to make a CD from music files.
 <h3>Log Files</h3>
 A <strong>log file</strong> is a text file with the file extension &#8220;.log&#8221;. Like its name suggests, it acts as a log of the entire ripping process and it records any errors that may have occurred. You may not, for any reason, modify a log file. It is strictly against the rules and you will be warned for a very long time. Click <a title="Sample Log File" href="log-file.php" target="_blank">here</a> to see an example of a log file.
 <h3>Cue Files</h3>
