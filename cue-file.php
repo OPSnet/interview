16f932cf97e8f10d7a11af4b009993e15cfb44f4
Aleš Švec <hawkeyetomatolover@gmail.com>
2023-07-14 00:37:28 +0200

    I have added <pre></pre> tag into the html to improve readability of the 2 pages.

--- cue-file.php
+++ cue-file.php
@@ -4,119 +4,136 @@
 
 newstop();
 
-newsbox('Sample cue file', '<code>REM GENRE Classical<br />
-REM DATE 1986<br />
-REM DISCID D20AFB11<br />
-REM COMMENT "ExactAudioCopy v1.0b3"<br />
-CATALOG 0028941446022<br />
-PERFORMER "Cristina Ortiz, Pascal Roge, London Sinfonietta, Philharmonia Orchestra, Char..."<br />
-TITLE "Carnaval Des Animaux, Danse Macabre, etc."</code>
-FILE &#8220;01 The Carnival of the Animals &#8211; Introduction and Royal March of the Lion.wav&#8221; WAVE<br />
-TRACK 01 AUDIO<br />
-TITLE &#8220;The Carnival of the Animals &#8211; Introduction and Royal March of the Lion&#8221;<br />
-PERFORMER &#8220;Cristina Ortiz, Pascal Roge, London Sinfonietta, Philharmonia Orchestra, Char&#8230;&#8221;<br />
-ISRC GBF078040301<br />
-PREGAP 00:00:32<br />
-INDEX 01 00:00:00
-FILE &#8220;02 The Carnival of the Animals &#8211; Hens and Cockerels.wav&#8221; WAVE<br />
-TRACK 02 AUDIO<br />
-TITLE &#8220;The Carnival of the Animals &#8211; Hens and Cockerels&#8221;<br />
-PERFORMER &#8220;Cristina Ortiz, Pascal Roge, London Sinfonietta, Philharmonia Orchestra, Char&#8230;&#8221;<br />
-ISRC GBF078040302<br />
-INDEX 01 00:00:00
-FILE &#8220;03 The Carnival of the Animals &#8211; Wild Asses.wav&#8221; WAVE<br />
-TRACK 03 AUDIO<br />
-TITLE &#8220;The Carnival of the Animals &#8211; Wild Asses&#8221;<br />
-PERFORMER &#8220;Cristina Ortiz, Pascal Roge, London Sinfonietta, Philharmonia Orchestra, Char&#8230;&#8221;<br />
-ISRC GBF078040303<br />
-INDEX 01 00:00:00
-FILE &#8220;04 The Carnival of the Animals &#8211; Tortoises.wav&#8221; WAVE<br />
-TRACK 04 AUDIO<br />
-TITLE &#8220;The Carnival of the Animals &#8211; Tortoises&#8221;<br />
-PERFORMER &#8220;Cristina Ortiz, Pascal Roge, London Sinfonietta, Philharmonia Orchestra, Char&#8230;&#8221;<br />
-ISRC GBF078040304<br />
-INDEX 01 00:00:00
-FILE &#8220;05 The Carnival of the Animals &#8211; The Elephant.wav&#8221; WAVE<br />
-TRACK 05 AUDIO<br />
-TITLE &#8220;The Carnival of the Animals &#8211; The Elephant&#8221;<br />
-PERFORMER &#8220;Cristina Ortiz, Pascal Roge, London Sinfonietta, Philharmonia Orchestra, Char&#8230;&#8221;<br />
-ISRC GBF078040305<br />
-INDEX 01 00:00:00
-FILE &#8220;06 The Carnival of the Animals &#8211; Kangaroos.wav&#8221; WAVE<br />
-TRACK 06 AUDIO<br />
-TITLE &#8220;The Carnival of the Animals &#8211; Kangaroos&#8221;<br />
-PERFORMER &#8220;Cristina Ortiz, Pascal Roge, London Sinfonietta, Philharmonia Orchestra, Char&#8230;&#8221;<br />
-ISRC GBF078040306<br />
-INDEX 01 00:00:00
-FILE &#8220;07 The Carnival of the Animals &#8211; Aquarium.wav&#8221; WAVE<br />
-TRACK 07 AUDIO<br />
-TITLE &#8220;The Carnival of the Animals &#8211; Aquarium&#8221;<br />
-PERFORMER &#8220;Cristina Ortiz, Pascal Roge, London Sinfonietta, Philharmonia Orchestra, Char&#8230;&#8221;<br />
-ISRC GBF078040307<br />
-INDEX 01 00:00:00
-FILE &#8220;08 The Carnival of the Animals &#8211; Persons With Long Ears.wav&#8221; WAVE<br />
-TRACK 08 AUDIO<br />
-TITLE &#8220;The Carnival of the Animals &#8211; Persons With Long Ears&#8221;<br />
-PERFORMER &#8220;Cristina Ortiz, Pascal Roge, London Sinfonietta, Philharmonia Orchestra, Char&#8230;&#8221;<br />
-ISRC GBF078040308<br />
-INDEX 01 00:00:00
-FILE &#8220;09 The Carnival of the Animals &#8211; The Cuckoo In the Depths of the Woods.wav&#8221; WAVE<br />
-TRACK 09 AUDIO<br />
-TITLE &#8220;The Carnival of the Animals &#8211; The Cuckoo In the Depths of the Woods&#8221;<br />
-PERFORMER &#8220;Cristina Ortiz, Pascal Roge, London Sinfonietta, Philharmonia Orchestra, Char&#8230;&#8221;<br />
-ISRC GBF078040309<br />
-INDEX 01 00:00:00
-FILE &#8220;10 The Carnival of the Animals &#8211; Aviary.wav&#8221; WAVE<br />
-TRACK 10 AUDIO<br />
-TITLE &#8220;The Carnival of the Animals &#8211; Aviary&#8221;<br />
-PERFORMER &#8220;Cristina Ortiz, Pascal Roge, London Sinfonietta, Philharmonia Orchestra, Char&#8230;&#8221;<br />
-ISRC GBF078040310<br />
-INDEX 01 00:00:00
-FILE &#8220;11 The Carnival of the Animals &#8211; Pianists.wav&#8221; WAVE<br />
-TRACK 11 AUDIO<br />
-TITLE &#8220;The Carnival of the Animals &#8211; Pianists&#8221;<br />
-PERFORMER &#8220;Cristina Ortiz, Pascal Roge, London Sinfonietta, Philharmonia Orchestra, Char&#8230;&#8221;<br />
-ISRC GBF078040311<br />
-INDEX 01 00:00:00
-FILE &#8220;12 The Carnival of the Animals &#8211; Fossils.wav&#8221; WAVE<br />
-TRACK 12 AUDIO<br />
-TITLE &#8220;The Carnival of the Animals &#8211; Fossils&#8221;<br />
-PERFORMER &#8220;Cristina Ortiz, Pascal Roge, London Sinfonietta, Philharmonia Orchestra, Char&#8230;&#8221;<br />
-ISRC GBF078040312<br />
-INDEX 01 00:00:00
-FILE &#8220;13 The Carnival of the Animals &#8211; The Swam.wav&#8221; WAVE<br />
-TRACK 13 AUDIO<br />
-TITLE &#8220;The Carnival of the Animals &#8211; The Swam&#8221;<br />
-PERFORMER &#8220;Cristina Ortiz, Pascal Roge, London Sinfonietta, Philharmonia Orchestra, Char&#8230;&#8221;<br />
-ISRC GBF078040313<br />
-INDEX 01 00:00:00
-FILE &#8220;14 The Carnival of the Animals &#8211; Finale.wav&#8221; WAVE<br />
-TRACK 14 AUDIO<br />
-TITLE &#8220;The Carnival of the Animals &#8211; Finale&#8221;<br />
-PERFORMER &#8220;Cristina Ortiz, Pascal Roge, London Sinfonietta, Philharmonia Orchestra, Char&#8230;&#8221;<br />
-ISRC GBF078040314<br />
-INDEX 01 00:00:00
-TRACK 15 AUDIO<br />
-TITLE &#8220;Phaeton, Op.39&#8243;<br />
-PERFORMER &#8220;Cristina Ortiz, Pascal Roge, London Sinfonietta, Philharmonia Orchestra, Char&#8230;&#8221;<br />
-ISRC GBF078010130<br />
-INDEX 00 01:53:52<br />
-FILE &#8220;15 Phaeton, Op.39.wav&#8221; WAVE<br />
-INDEX 01 00:00:00
-TRACK 16 AUDIO<br />
-TITLE &#8220;Le Rouet d&#8217;Omphale, Op. 31&#8243;<br />
-PERFORMER &#8220;Cristina Ortiz, Pascal Roge, London Sinfonietta, Philharmonia Orchestra, Char&#8230;&#8221;<br />
-ISRC GBF078010140<br />
-INDEX 00 08:59:50<br />
-FILE &#8220;16 Le Rouet d&#8217;Omphale, Op. 31.wav&#8221; WAVE<br />
-INDEX 01 00:00:00
-TRACK 17 AUDIO<br />
-TITLE &#8220;Danse Macabre, Op. 40&#8243;<br />
-PERFORMER &#8220;Cristina Ortiz, Pascal Roge, London Sinfonietta, Philharmonia Orchestra, Char&#8230;&#8221;<br />
-ISRC GBF078010150<br />
-INDEX 00 07:41:43<br />
-FILE &#8220;17 Danse Macabre, Op. 40.wav&#8221; WAVE<br />
-INDEX 01 00:00:00');
+newsbox('Sample cue file', '<pre>REM GENRE Classical
+REM DATE 1986
+REM DISCID D20AFB11
+REM COMMENT "ExactAudioCopy v1.0b3"
+CATALOG 0028941446022
+PERFORMER "Cristina Ortiz, Pascal Roge, London Sinfonietta, Philharmonia Orchestra, Char..."
+TITLE "Carnaval Des Animaux, Danse Macabre, etc."
+
+FILE "01 The Carnival of the Animals - Introduction and Royal March of the Lion.wav" WAVE
+  TRACK 01 AUDIO
+    TITLE "The Carnival of the Animals - Introduction and Royal March of the Lion"
+    PERFORMER "Cristina Ortiz, Pascal Roge, London Sinfonietta, Philharmonia Orchestra, Char..."
+    ISRC GBF078040301
+    PREGAP 00:00:32
+    INDEX 01 00:00:00
+
+FILE "02 The Carnival of the Animals - Hens and Cockerels.wav" WAVE
+  TRACK 02 AUDIO
+    TITLE "The Carnival of the Animals - Hens and Cockerels"
+    PERFORMER "Cristina Ortiz, Pascal Roge, London Sinfonietta, Philharmonia Orchestra, Char..."
+    ISRC GBF078040302
+    INDEX 01 00:00:00
+
+FILE "03 The Carnival of the Animals - Wild Asses.wav" WAVE
+  TRACK 03 AUDIO
+    TITLE "The Carnival of the Animals - Wild Asses"
+    PERFORMER "Cristina Ortiz, Pascal Roge, London Sinfonietta, Philharmonia Orchestra, Char..."
+    ISRC GBF078040303
+    INDEX 01 00:00:00
+
+FILE "04 The Carnival of the Animals - Tortoises.wav" WAVE
+  TRACK 04 AUDIO
+    TITLE "The Carnival of the Animals - Tortoises"
+    PERFORMER "Cristina Ortiz, Pascal Roge, London Sinfonietta, Philharmonia Orchestra, Char..."
+    ISRC GBF078040304
+    INDEX 01 00:00:00
+
+FILE "05 The Carnival of the Animals - The Elephant.wav" WAVE
+  TRACK 05 AUDIO
+    TITLE "The Carnival of the Animals - The Elephant"
+    PERFORMER "Cristina Ortiz, Pascal Roge, London Sinfonietta, Philharmonia Orchestra, Char..."
+    ISRC GBF078040305
+    INDEX 01 00:00:00
+
+FILE "06 The Carnival of the Animals - Kangaroos.wav" WAVE
+  TRACK 06 AUDIO
+    TITLE "The Carnival of the Animals - Kangaroos"
+    PERFORMER "Cristina Ortiz, Pascal Roge, London Sinfonietta, Philharmonia Orchestra, Char..."
+    ISRC GBF078040306
+    INDEX 01 00:00:00
+
+FILE "07 The Carnival of the Animals - Aquarium.wav" WAVE
+  TRACK 07 AUDIO
+    TITLE "The Carnival of the Animals - Aquarium"
+    PERFORMER "Cristina Ortiz, Pascal Roge, London Sinfonietta, Philharmonia Orchestra, Char..."
+    ISRC GBF078040307
+    INDEX 01 00:00:00
+
+FILE "08 The Carnival of the Animals - Persons With Long Ears.wav" WAVE
+  TRACK 08 AUDIO
+    TITLE "The Carnival of the Animals - Persons With Long Ears"
+    PERFORMER "Cristina Ortiz, Pascal Roge, London Sinfonietta, Philharmonia Orchestra, Char..."
+    ISRC GBF078040308
+    INDEX 01 00:00:00
+
+FILE "09 The Carnival of the Animals - The Cuckoo In the Depths of the Woods.wav" WAVE
+  TRACK 09 AUDIO
+    TITLE "The Carnival of the Animals - The Cuckoo In the Depths of the Woods"
+    PERFORMER "Cristina Ortiz, Pascal Roge, London Sinfonietta, Philharmonia Orchestra, Char..."
+    ISRC GBF078040309
+    INDEX 01 00:00:00
+
+FILE "10 The Carnival of the Animals - Aviary.wav" WAVE
+  TRACK 10 AUDIO
+    TITLE "The Carnival of the Animals - Aviary"
+    PERFORMER "Cristina Ortiz, Pascal Roge, London Sinfonietta, Philharmonia Orchestra, Char..."
+    ISRC GBF078040310
+    INDEX 01 00:00:00
+
+FILE "11 The Carnival of the Animals - Pianists.wav" WAVE
+  TRACK 11 AUDIO
+    TITLE "The Carnival of the Animals - Pianists"
+    PERFORMER "Cristina Ortiz, Pascal Roge, London Sinfonietta, Philharmonia Orchestra, Char..."
+    ISRC GBF078040311
+    INDEX 01 00:00:00
+
+FILE "12 The Carnival of the Animals - Fossils.wav" WAVE
+  TRACK 12 AUDIO
+    TITLE "The Carnival of the Animals - Fossils"
+    PERFORMER "Cristina Ortiz, Pascal Roge, London Sinfonietta, Philharmonia Orchestra, Char..."
+    ISRC GBF078040312
+    INDEX 01 00:00:00
+
+FILE "13 The Carnival of the Animals - The Swam.wav" WAVE
+  TRACK 13 AUDIO
+    TITLE "The Carnival of the Animals - The Swam"
+    PERFORMER "Cristina Ortiz, Pascal Roge, London Sinfonietta, Philharmonia Orchestra, Char..."
+    ISRC GBF078040313
+    INDEX 01 00:00:00
+
+FILE "14 The Carnival of the Animals - Finale.wav" WAVE
+  TRACK 14 AUDIO
+    TITLE "The Carnival of the Animals - Finale"
+    PERFORMER "Cristina Ortiz, Pascal Roge, London Sinfonietta, Philharmonia Orchestra, Char..."
+    ISRC GBF078040314
+    INDEX 01 00:00:00
+
+FILE "15 Phaeton, Op.39.wav" WAVE
+  TRACK 15 AUDIO
+    TITLE "Phaeton, Op.39"
+    PERFORMER "Cristina Ortiz, Pascal Roge, London Sinfonietta, Philharmonia Orchestra, Char..."
+    ISRC GBF078010130
+    INDEX 00 01:53:52
+    INDEX 01 00:00:00
+
+FILE "16 Le Rouet d\'Omphale, Op. 31.wav" WAVE
+  TRACK 16 AUDIO
+    TITLE "Le Rouet d\'Omphale, Op. 31"
+    PERFORMER "Cristina Ortiz, Pascal Roge, London Sinfonietta, Philharmonia Orchestra, Char..."
+    ISRC GBF078010140
+    INDEX 00 08:59:50
+    INDEX 01 00:00:00
+
+FILE "17 Danse Macabre, Op. 40.wav" WAVE
+  TRACK 17 AUDIO
+    TITLE "Danse Macabre, Op. 40"
+    PERFORMER "Cristina Ortiz, Pascal Roge, London Sinfonietta, Philharmonia Orchestra, Char..."
+    ISRC GBF078010150
+    INDEX 00 07:41:43
+    INDEX 01 00:00:00</pre>');
 
 newsbot();
 
